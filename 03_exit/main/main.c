80565ef64a71fa51ee74803846d26d0a2788984a
veaba <godpu@outlook.com>
2026-05-21 23:07:13 +0800

    feat: add 03_exit project

---
+++ 03_exit/main/main.c
@@ -0,0 +1,15 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "led.h"
+#include "exit.h"
+
+void app_main(void)
+{
+  led_init();
+  exit_init();
+
+  while (1)
+  {
+    vTaskDelay(10); // 延时100ms，避免按键抖动
+  }
+}
